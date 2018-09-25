7232a5ce0ea3dc19a5064ef302c6c9d1453f6a48
nsom <somavarapun@gmail.com>
2018-09-25 21:11:48 +0000

    Added updated model, need to add dataloader.

--- ssd.py
+++ ssd.py
@@ -14,13 +14,23 @@ class ssd(nn.Module):
 
         self.f1 = nn.Sequential(*new_layers[:23])
 
+        self.cl1 = nn.Sequential(
+            nn.Conv2d(512, 4*(num_cl + 4), 3),
+            nn.ReLU(inplace=True)
+        )
+
         self.base1 = nn.Sequential(*new_layers[23:])
 
         # The refrence code uses  a dilation of 6 which requires a padding of 6
         self.f2 = nn.Sequential(
-            nn.Conv2d(512, 1024, 3, stride=1, dilation=3, padding=1), 
+            nn.Conv2d(512, 1024, 3, dilation=3, padding=1), 
             nn.ReLU(inplace=True), 
-            nn.Conv2d(1024, 1024, 1, stride=0), 
+            nn.Conv2d(1024, 1024, 1), 
+            nn.ReLU(inplace=True)
+        )
+
+        self.cl2 = nn.Sequential(
+            nn.Conv2d(1024, 6*(num_cl + 4), 3),
             nn.ReLU(inplace=True)
         )
 
@@ -34,7 +44,7 @@ class ssd(nn.Module):
         self.f4 = nn.Sequential(
             nn.Conv2d(512, 128, 1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(128, 256, 3, padding=1),
+            nn.Conv2d(128, 256, 3, stride=2, padding=1), # This padding is likely wrong
             nn.ReLU(inplace=True)
         )
 
@@ -45,23 +55,46 @@ class ssd(nn.Module):
             nn.ReLU(inplace=True)
         )
 
-        self.cl1 = nn.Sequential(
-            nn.Conv2d(512, 4*(num_cl + 4), 3),
-            nn.ReLU(inplace=True)
-        )
-        
-        self.cl2 = nn.Sequential(
-            nn.Conv2d(1024, 6*(num_cl + 4), 3),
-            nn.ReLU(inplace=True)
-        )
-
         self.cl5 = nn.Sequential(
-            nn.Conv2d(512, 4*(num_cl + 4), 3),
+            nn.Conv2d(256, 4*(num_cl + 4), 3),
             nn.ReLU(inplace=True)
         )
-    
+
+        self.f6 = nn.Sequential(
+            nn.Conv2d(256, 128, 1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+        
     def forward(self, x):
-        pass
+
+        x1 = self.f1(x)
+        x1_2 = self.cl1(x1)
+
+        x1 = self.base1(x1)
+
+        x2 = self.f2(x1)
+        x2_2 = self.cl2(x2)
+
+        x3 = self.f3(x2)
+
+        x4 = self.f4(x3)
+
+        x5 = self.f5(x4)
+        x5_2 = self.cl5(x5)
+
+        x6 = self.f6(x5)
+
+        return torch.cat([x1_2, x2_2, x3, x4, x5_2, x6], dim=1)
 
 
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 model = ssd(10)
+model = model.to(device)
+
+x = torch.zeros((1, 3, 300, 300))
+x = x.to(device)
+
+print(model(x))
