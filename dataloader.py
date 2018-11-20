aa13195a10d1ad781334fcb66d694fc025f04516
nsom <Somavarapun@gmail.com>
2018-11-20 01:01:48 -0500

    Validated matching step.

--- dataloader.py
+++ dataloader.py
@@ -13,196 +13,176 @@ from pycocotools.coco import COCO
 
 class LocData(Dataset):
 
-    def __init__(self, ann_path, img_path, data_type, name_path=None, size=(300,300), testing=False):
+	def __init__(self, ann_path, img_path, data_type, name_path=None, size=(300,300), testing=False):
 
-        self.data_type = data_type
-        self.data = []
-        self.size = size
-        self.testing = testing
-        if self.data_type == 'VOC':
-            assert name_path is not None
+		self.data_type = data_type
+		self.data = []
+		self.size = size
+		self.testing = testing
+		if self.data_type == 'VOC':
+			assert name_path is not None
 
-            ann_paths = glob.glob(os.path.join(ann_path, '*'))
-            imgs = glob.glob(os.path.join(img_path, '*'))
+			ann_paths = glob.glob(os.path.join(ann_path, '*'))
+			imgs = glob.glob(os.path.join(img_path, '*'))
 
-            img_ext = imgs[0].split('.')[-1]
-            for ann in ann_paths:
-                ann_ext = ann.split('.')[-1]
-                check_img = ann.split('/')[-1]
-                check_img = check_img.replace(ann_ext, img_ext)
-                check_img = os.path.join(img_path, check_img)
+			img_ext = imgs[0].split('.')[-1]
+			for ann in ann_paths:
+				ann_ext = ann.split('.')[-1]
+				check_img = ann.split('/')[-1]
+				check_img = check_img.replace(ann_ext, img_ext)
+				check_img = os.path.join(img_path, check_img)
 
-                if os.path.exists(check_img):
-                    self.data.append((ann, check_img))
-        
-            self.nametoint = {}
-            nf = open(name_path, 'r')
-            for i, name in enumerate(nf.readlines()):
-                self.nametoint[name.strip()] = int(i)
-            nf.close()
-        elif self.data_type == 'COCO':
-            self.coco=COCO(ann_path)
-            self.imgs = self.coco.getImgIds()
-            self.img_path = img_path
-            self.coco_cats = []
-            self.cat_renum_dict = {}
+				if os.path.exists(check_img):
+					self.data.append((ann, check_img))
+		
+			self.nametoint = {}
+			nf = open(name_path, 'r')
+			for i, name in enumerate(nf.readlines()):
+				self.nametoint[name.strip()] = int(i)
+			nf.close()
+		elif self.data_type == 'COCO':
+			self.coco=COCO(ann_path)
+			self.imgs = self.coco.getImgIds()
+			self.img_path = img_path
+			self.coco_cats = []
+			self.cat_renum_dict = {}
 
-            for i, cat_dict in enumerate(self.coco.dataset['categories']):
-                self.coco_cats.append(cat_dict['name'])
-                self.cat_renum_dict[cat_dict['id']] = i
-            
-            self.coco_cats.append('None')
+			for i, cat_dict in enumerate(self.coco.dataset['categories']):
+				self.coco_cats.append(cat_dict['name'])
+				self.cat_renum_dict[cat_dict['id']] = i
+			
+			self.coco_cats.append('None')
 
 
-        elif self.data_type == 'YOLO':
-            raise NotImplementedError()
+		elif self.data_type == 'YOLO':
+			raise NotImplementedError()
 
-    def __len__(self):
-        if self.data_type == 'VOC':
-            return len(self.data)
-        elif self.data_type == 'COCO':
-            return len(self.imgs)
-    
-    def __getitem__(self, index):
-        ann_repr = []
-        img = None
-        # Approx VOC format
-        if self.data_type == 'VOC':
-            curr_ann, curr_img = self.data[index]
-            xml_f = open(curr_ann, 'r')
-            root = etree.XML(xml_f.read())
-            xml_f.close()
+	def __len__(self):
+		if self.data_type == 'VOC':
+			return len(self.data)
+		elif self.data_type == 'COCO':
+			return len(self.imgs)
+	
+	def __getitem__(self, index):
+		ann_repr = []
+		img = None
+		# Approx VOC format
+		if self.data_type == 'VOC':
+			curr_ann, curr_img = self.data[index]
+			xml_f = open(curr_ann, 'r')
+			root = etree.XML(xml_f.read())
+			xml_f.close()
 
-            objs = root.findall('object')
-            for obj in objs:
-                cl = self.nametoint[obj.find('name').text]
-                _bbx = obj.find('bndbox')
-                x1 = int(_bbx.find('xmin'))
-                x2 = int(_bbx.find('xmax'))
-                y1 = int(_bbx.find('ymin'))
-                y2 = int(_bbx.find('ymax'))
+			objs = root.findall('object')
+			for obj in objs:
+				cl = self.nametoint[obj.find('name').text]
+				_bbx = obj.find('bndbox')
+				x1 = int(_bbx.find('xmin'))
+				x2 = int(_bbx.find('xmax'))
+				y1 = int(_bbx.find('ymin'))
+				y2 = int(_bbx.find('ymax'))
 
-                w = x2 - x1
-                h = y2 - y1
+				w = x2 - x1
+				h = y2 - y1
 
-                assert w > 0 and h > 0
+				assert w > 0 and h > 0
 
-                cx = x1 + w/2.0
-                cy = y1 + h/2.0
+				cx = x1 + w/2.0
+				cy = y1 + h/2.0
 
-                ann_repr.append([cl, cx, cy, w, h])
-            img = cv2.imread(curr_img)
-        elif self.data_type == 'COCO':
-            curr_img_id = self.imgs[index]
-            ann_ids = self.coco.getAnnIds(imgIds=curr_img_id)
-            anns = self.coco.loadAnns(ann_ids)
-            img_f = self.coco.loadImgs(curr_img_id)
+				ann_repr.append([cl, cx, cy, w, h])
+			img = cv2.imread(curr_img)
+		elif self.data_type == 'COCO':
+			curr_img_id = self.imgs[index]
+			ann_ids = self.coco.getAnnIds(imgIds=curr_img_id)
+			anns = self.coco.loadAnns(ann_ids)
+			img_f = self.coco.loadImgs(curr_img_id)
 
-            assert len(img_f) == 1
+			assert len(img_f) == 1
 
-            img_f = img_f[0]['file_name']
-            for ann in anns:
-                cl = ann['category_id']
-                _bbx = ann['bbox'] # Check what format this is in
+			img_f = img_f[0]['file_name']
+			for ann in anns:
+				cl = ann['category_id']
+				_bbx = ann['bbox'] # Check what format this is in
 
-                x1, y1, w, h = tuple(_bbx)
+				x1, y1, w, h = tuple(_bbx)
 
-                _bbx = [x1 + w/2.0, y1 + h/2.0, w, h]
-                
-                ann_repr.append([self.cat_renum_dict[cl]] + _bbx)
+				_bbx = [x1 + w/2.0, y1 + h/2.0, w, h]
+				
+				ann_repr.append([self.cat_renum_dict[cl]] + _bbx)
 
-            img = cv2.imread(os.path.join(self.img_path, img_f))
-            
-        elif self.data_type == 'YOLO': 
-            raise NotImplementedError()
-        
-        max_side = max(img.shape[:2])
+			img = cv2.imread(os.path.join(self.img_path, img_f))
+			
+		elif self.data_type == 'YOLO': 
+			raise NotImplementedError()
+		
+		max_side = max(img.shape[:2])
 
-        y_pad = int((max_side - img.shape[0])/2)
-        x_pad = int((max_side - img.shape[1])/2)
+		y_pad = int((max_side - img.shape[0])/2)
+		x_pad = int((max_side - img.shape[1])/2)
 
-        img = np.pad(img, ((y_pad, y_pad), (x_pad, x_pad), (0,0)), mode='constant', constant_values=0)
-        img_pad_s = img.shape
-        img = cv2.resize(img, self.size)
-        img_old = img.copy()
-        if not self.testing:
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            img = img.astype(np.float32)/255.0
+		img = np.pad(img, ((y_pad, y_pad), (x_pad, x_pad), (0,0)), mode='constant', constant_values=0)
+		img_pad_s = img.shape
+		img = cv2.resize(img, self.size)
+		img_old = img.copy()
+		
+		img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+		img = img.astype(np.float32)/255.0
 
-        ratio_x = img.shape[1]/float(img_pad_s[1])
-        ratio_y = img.shape[0]/float(img_pad_s[0])
+		ratio_x = img.shape[1]/float(img_pad_s[1])
+		ratio_y = img.shape[0]/float(img_pad_s[0])
 
-        # Perform any anotation corrections
-        for ann in ann_repr:
-            ann[1] += x_pad
-            ann[2] += y_pad
+		# Perform any anotation corrections
+		for ann in ann_repr:
+			ann[1] += x_pad
+			ann[2] += y_pad
 
-            ann[1] *= ratio_x
-            ann[2] *= ratio_y
-            ann[3] *= ratio_x
-            ann[4] *= ratio_y
+			ann[1] *= ratio_x
+			ann[2] *= ratio_y
+			ann[3] *= ratio_x
+			ann[4] *= ratio_y
 
-            ann[1] /= float(self.size[0])
-            ann[2] /= float(self.size[1])
+			ann[1] /= float(self.size[0])
+			ann[2] /= float(self.size[1])
 
-            ann[3] /= float(self.size[0])
-            ann[4] /= float(self.size[1])
+			ann[3] /= float(self.size[0])
+			ann[4] /= float(self.size[1])
 
-        img = torch.from_numpy(img.transpose(2,0,1)).float()
-        
-        if self.testing:
-            # print(img_f)
-            # print(np.array(ann_repr)[:,0])
-            img_h, img_w = img_old.shape[:2]
-            for ann in ann_repr:
-                point1 = (int((ann[1] - ann[3]/2.0) * img_w), int((ann[2] - ann[4]/2.0) * img_h))
-                point2 = (int((ann[1] + ann[3]/2.0) * img_w), int((ann[2] + ann[4]/2.0) * img_h))
-                cv2.rectangle(img_old, point1, point2, (0,255,0), 4)
-                cv2.putText(img_old, self.coco_cats[int(ann[0])], (point1[0],  point1[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+		img = torch.from_numpy(img.transpose(2,0,1)).float()
+		
+		if self.testing:
+			# print(img_f)
+			# print(np.array(ann_repr)[:,0])
+			img_h, img_w = img_old.shape[:2]
+			for ann in ann_repr:
+				point1 = (int((ann[1] - ann[3]/2.0) * img_w), int((ann[2] - ann[4]/2.0) * img_h))
+				point2 = (int((ann[1] + ann[3]/2.0) * img_w), int((ann[2] + ann[4]/2.0) * img_h))
+				cv2.rectangle(img_old, point1, point2, (0,255,0))
+				cv2.putText(img_old, self.coco_cats[int(ann[0])], (point1[0],  point1[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 4)
 
-            cv2.imwrite('img_w_anns.png', img_old)
+			cv2.imwrite('img_w_anns.png', img_old)
 
-        return img, ann_repr
-    
-    def get_categories(self):
-        if self.data_type == 'COCO':
-           return self.coco_cats
+		return img, ann_repr
+	
+	def get_categories(self):
+		if self.data_type == 'COCO':
+			return self.coco_cats
 
 def collate_fn_cust(data):
-        imgs, anns = zip(*data)
-        imgs = torch.stack(imgs, 0)
+		imgs, anns = zip(*data)
+		imgs = torch.stack(imgs, 0)
 
-        ann_lens = [len(ann) for ann in anns]
-        per_ann_len = len(anns[0][0])
-        max_ann_len = max(ann_lens)
+		ann_lens = [len(ann) for ann in anns]
+		per_ann_len = len(anns[0][0])
+		max_ann_len = max(ann_lens)
 
-        tmp = np.zeros((len(data), max_ann_len, per_ann_len))
+		tmp = np.zeros((len(data), max_ann_len, per_ann_len))
 
-        for ann_ind in range(len(anns)):
-            tmp[ann_ind,:ann_lens[ann_ind],:] = anns[ann_ind]
-        
-        anns = torch.from_numpy(tmp)
-        lengths = torch.from_numpy(np.array(ann_lens))
+		for ann_ind in range(len(anns)):
+			tmp[ann_ind,:ann_lens[ann_ind],:] = anns[ann_ind]
+		
+		anns = torch.from_numpy(tmp)
+		lengths = torch.from_numpy(np.array(ann_lens))
 
-        return imgs, anns, lengths
-
-# testing = False
-# if testing:
-#     traindata = LocData('../data/annotations/instances_train2017.json', '../data/train2017', 'COCO', testing=True)
-#     ind = np.random.randint(len(traindata))
-#     image, annotations, _ = traindata[ind]
-
-#     print(annotations)
-
-#     img_h, img_w = image.shape[:2]
-
-#     cat_to_names = traindata.get_categories()
-
-#     for ann in annotations:
-#         point1 = (int((ann[1] - ann[3]/2.0) * img_w), int((ann[2] - ann[4]/2.0) * img_h))
-#         point2 = (int((ann[1] + ann[3]/2.0) * img_w), int((ann[2] + ann[4]/2.0) * img_h))
-#         cv2.rectangle(image, point1, point2, (0,255,0), 4)
-#         cv2.putText(image, cat_to_names[int(ann[0])], (point1[0],  point1[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
-
-#     cv2.imwrite('img_w_anns.png', image)
+		return imgs, anns, lengths
 
--- ssd_trainer.py
+++ ssd_trainer.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.functional as F
 
 import torchvision
 from torch.utils.data import DataLoader
@@ -12,62 +13,88 @@ import torch.optim as optim
 import cv2
 
 # TODO: Change this to perform tensor operations.
-def gen_loss(def_bxs, ann_bxs, pred, lens, device, thresh=0.5):
+# This is going to need to be done one image at a time, batches wont work, uneven num_objs
+def gen_loss(def_bxs, ann_bxs, pred, lens, device, num_cats, thresh=0.5):
 
 	pred_cl = pred[0]
 	pred_reg = pred[1]
-	# Add lens here for accuracy
-	ann_cl = ann_bxs[:,:,0]
-	ann_cords = ann_bxs[:,:,1:5]
+	ann_cl = ann_bxs[:,0]
+	ann_cords = ann_bxs[:,1:5]
 
-	batch_size = ann_bxs.size(0)
-	num_dbx = def_bxs.size(1)
-	num_ann = ann_cords.size(1)
+	num_dbx = def_bxs.size(0)
+	num_ann = ann_cords.size(0)
 
 	def_mins = def_bxs[:,:2] - def_bxs[:,2:]/2.0
 	def_maxs = def_bxs[:,:2] + def_bxs[:,2:]/2.0
 
 	def_pts = torch.cat([def_mins, def_maxs], 1)
 
-	ann_mins = ann_cords[:,:,:2] - ann_cords[:,:,2:]/2.0
-	ann_maxs = ann_cords[:,:,:2] + ann_cords[:,:,2:]/2.0
+	ann_mins = ann_cords[:,:2] - ann_cords[:,2:]/2.0
+	ann_maxs = ann_cords[:,:2] + ann_cords[:,2:]/2.0
 
-	ann_pts = torch.cat([ann_mins, ann_maxs], 2)
+	ann_pts = torch.cat([ann_mins, ann_maxs], 1)
 
-	def_pts = def_pts.unsqueeze(0).unsqueeze(0)
-	ann_pts = ann_pts.unsqueeze(2)
+	def_pts = def_pts.unsqueeze(0)
+	ann_pts = ann_pts.unsqueeze(1)
 
-	def_pts = def_pts.expand(batch_size, num_ann, -1, -1)
+	def_pts = def_pts.expand(num_ann, -1, -1)
 	ann_pts = ann_pts.expand_as(def_pts)
 	
-	mins = torch.min(def_pts[:,:,:,2:], ann_pts[:,:,:,2:])
-	maxs = torch.max(def_pts[:,:,:,:2], ann_pts[:,:,:,:2])
+	mins = torch.min(def_pts[:,:,2:], ann_pts[:,:,2:])
+	maxs = torch.max(def_pts[:,:,:2], ann_pts[:,:,:2])
 
 	diff = torch.clamp(mins - maxs, min=0)
-	intersect = diff[:,:,:,0] * diff[:,:,:,1]
+	intersect = diff[:,:,0] * diff[:,:,1]
 
-	def_diff = def_pts[:,:,:,2:] - def_pts[:,:,:,:2]
-	def_area = def_diff[:,:,:,0] * def_diff[:,:,:,1]
+	def_diff = def_pts[:,:,2:] - def_pts[:,:,:2]
+	def_area = def_diff[:,:,0] * def_diff[:,:,1]
 
-	ann_diff = ann_pts[:,:,:,2:] - ann_pts[:,:,:,:2]
-	ann_area = ann_diff[:,:,:,0] * ann_diff[:,:,:,1]
+	ann_diff = ann_pts[:,:,2:] - ann_pts[:,:,:2]
+	ann_area = ann_diff[:,:,0] * ann_diff[:,:,1]
 
 	ious = intersect / (def_area + ann_area - intersect)
 
+	max_defs, max_def_inds = torch.max(ious, 0)
 	max_anns, max_ann_inds = torch.max(ious, 1)
-	max_defs, max_def_inds = torch.max(ious, 2)
+	cl_nums = torch.arange(max_anns.size(0), dtype=torch.long)
 
+	g = torch.ones((num_dbx, 4), dtype=torch.float)
 
-	print(num_ann)
-	print(max_anns.size())
-	print(max_defs.size())
+	max_def_inds[max_ann_inds] = cl_nums
 
+	match_inds = max_defs >= thresh
+	match_inds[max_ann_inds] = 1
 
-	return None, None
+	g[match_inds] = ann_cords[max_def_inds[match_inds]].float()
+
+	N = torch.sum(match_inds)
+
+	diff_c = (g[:,:2] - def_bxs.float()[:,:2])/def_bxs.float()[:,2:]
+	diff_wh = torch.log(g[:,2:]/def_bxs.float()[:,2:])
+	gt = torch.cat([diff_c, diff_wh], 1)
+
+	loc_criterion = nn.SmoothL1Loss(reduce=False)
+	loc_loss = loc_criterion(pred_reg[match_inds], gt[match_inds])
+	loc_loss = loc_loss.sum(1)
+	loc_loss = loc_loss.sum(0)
+
+	cl_criterion = nn.CrossEntropyLoss(reduce=False)
+	cl = torch.ones((num_dbx), dtype=torch.long) * num_cats
+	cl[match_inds] = ann_cl[max_def_inds[match_inds]].long()
+
+	cl_loss = cl_criterion(pred_cl, cl)
+	cl_pos = cl_loss[cl != num_cats]
+	cl_neg = cl_loss[cl == num_cats]
+	hnegs = torch.topk(cl_neg, N.item() * 3)[0]
+	cl_loss = torch.cat([cl_pos, cl_neg], 0).sum(0)
+	
+	total_loss = loc_loss + cl_loss
+	
+	return (total_loss)/N.item()
 
 def main():
 
-	trainset = LocData('../data/annotations/instances_train2017.json', '../data/train2017', 'COCO', testing=True)
+	trainset = LocData('../data/annotations/instances_train2017.json', '../data/train2017', 'COCO', testing=False)
 	trainloader = DataLoader(trainset, batch_size=1, shuffle=True, collate_fn=collate_fn_cust)
 
 	device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -77,39 +104,29 @@ def main():
 	default_boxes = model._get_pboxes()
 	default_boxes = default_boxes.to(device)
 
-	print(default_boxes.size())
-
-	# opt = optim.SGD(model.parameters(), lr=0.001)
+	opt = optim.SGD(model.parameters(), lr=0.001)
 
 	for i, data in enumerate(trainloader):
-		img, anns_gt, lens = data
+		imgs, anns_gt, lens = data
 
-		# cv2.imwrite('curr_debug.png', img_old)
-
-		img = img.to(device)
+		imgs = imgs.to(device)
 		anns_gt = anns_gt.to(device)
 		lens = lens.to(device)
 
-		pred = model(img)
+		preds = model(imgs)
 
-		# print(anns_gt)
-		# print(pred[0].size(), pred[1].size())
-
-		loss, diagnostics = gen_loss(default_boxes, anns_gt, pred, lens, device)
-	# 	print(loss)
-	# 	for bbx in default_boxes[diagnostics.byte()]:
-	# 		img_old = cv2.rectangle(img_old, ((bbx[0] - bbx[2]) * img_old.shape[1], (bbx[1] - bbx[3]) * img_old.shape[0]), ((bbx[0] + bbx[2]) * img_old.shape[1], (bbx[1] + bbx[3]) * img_old.shape[0]), (255,0,0))
-		
-	# 	for ann in anns_gt[0]:
-	# 		bbx = ann[1:]
-	# 		img_old = cv2.rectangle(img_old, ((bbx[0] - bbx[2]) * img_old.shape[1], (bbx[1] - bbx[3]) * img_old.shape[0]), ((bbx[0] + bbx[2]) * img_old.shape[1], (bbx[1] + bbx[3]) * img_old.shape[0]), (0,255,0))
-
-	# 	cv2.imwrite('img_w_def_anns.png', img_old)
-	# 	opt.zero_grad()
-	# 	loss.backward()
-	# 	opt.step()
-
-
+		batch_loss = 0
+		for i in range(anns_gt.size(0)):
+			ann_gt = anns_gt[i]
+			pred = (preds[0][i], preds[1][i])
+			loss = gen_loss(default_boxes, ann_gt, pred, lens, device, len(trainset.get_categories()))
+			batch_loss += loss
+			print(batch_loss)
+			
+		opt.zero_grad()
+		batch_loss.backward()
+		opt.step()
 		break
+
 if __name__ == '__main__':
 	main()
